ce01548394c68f0c35f94e1e6110ce65b4cfadd9
deftlypatrick <wongpatrick@gmail.com>
2021-06-17 23:03:06 -0700

    new

--- fundamentals part 2/functions.js
+++ fundamentals part 2/functions.js
@@ -1,27 +1,44 @@
 
 
-function add7(number){
+// function add7(number){
+//     return number + 7;
+// }
+
+var add7 = (number) =>{
     return number + 7;
 }
 
 console.log(add7(18));
 
-function multiply(a, b){
-    return a * b;
+// function multiply(a, b){
+//     return a * b;
+// }
+
+var multiply = (a, b) => {
+    return a * b 
 }
+
 
 console.log(multiply(2,5));
 
-function capitalize(str){
+// function capitalize(str){
+//     str = str.toLowerCase();
+//     return str.charAt(0).toUpperCase() + str.substring(1, str.length) ;
+// }
+
+var capitalize = (str) =>{
     str = str.toLowerCase();
-    return str.charAt(0).toUpperCase() + str.substring(1, str.length) ;
+    return str.charAt(0).toUpperCase() + str.substring(1, str.length);
 }
 
 console.log(capitalize("Hello World"));
 
+// function lastString(str){
+//     return str.charAt(str.length-1);
+// }
 
-function lastString(str){
-    return str.charAt(str.length-1);
+var lastString = (str) => {
+    return str.charAt(str.length - 1);
 }
 
 console.log(lastString("Hello World"));
